2acd427c6ce7f6cd3b15241dc45abeac2e7ae183
Asit-kumar-Raut <asitraut2006@gmail.com>
2026-04-26 11:03:38 +0530

    Fixing build-time crash due to missing MONGODB_URI

--- src/lib/db.js
+++ src/lib/db.js
@@ -9,19 +9,23 @@ const options = {
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+if (!process.env.MONGODB_URI && process.env.NODE_ENV === 'production') {
+  console.warn('Warning: MONGODB_URI is missing. Database features will fail at runtime.');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
+if (process.env.MONGODB_URI) {
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    clientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  clientPromise = Promise.reject(new Error('MONGODB_URI is not defined'));
 }
 
 export default clientPromise;
